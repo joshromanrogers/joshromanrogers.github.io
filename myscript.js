39b24314e851af85a7e32930a50e6d81c06097af
Josh Rogers <joshromanrogers@gmail.com>
2018-02-11 12:28:10 +0100

    Carousel Updates
     JavaScript functionality updates (indicators). CSS update - e-resize
    cursor on hover.

--- myscript.js
+++ myscript.js
@@ -1,13 +1,21 @@
+  /* variables */
 
   var box = document.querySelector('.carouselbox');
-  var next = box.querySelector('.next');
+  var next = box;
   var items = box.querySelectorAll('.content li');
   var counter = 0;
   var amount = items.length;
   var current = items[0];
   box.classList.add('active');
 
-function nextSlide() {
+  var indicators = document.querySelectorAll('.indicatorItem');
+  var currentIndicator = indicators[0];
+
+
+
+  /* functions */
+
+  function nextSlide() {
     current.classList.remove('current');
     counter++;
     if(counter == amount) {
@@ -17,6 +25,18 @@ function nextSlide() {
     current.classList.add('current');
   }
 
+  function nextIndicator() {
+    currentIndicator.classList.remove('active');
+    currentIndicator = indicators[counter];
+    currentIndicator.classList.add('active');
+  }
+
+  /* event listeners */
+
   next.addEventListener('click', function() {
     nextSlide();
+  });
+
+next.addEventListener('click', function() {
+    nextIndicator();
   });
